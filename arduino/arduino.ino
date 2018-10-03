d1f631c8f351ad5a4b305ea1274c04be0367f077
Nelson Quek <>
2018-10-03 16:49:40 +0800

    updated cTaskDelay and add serial1.flush()

--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -150,8 +150,9 @@ void task1(void *p) {
         // Increment frameID and Re-initialize checksum
         frameID++;
         checksum = 0;     
+        vTaskDelayUntil(&xLastWakeTime, xFrequency);
       }
-      vTaskDelayUntil(&xLastWakeTime, xFrequency);
+      Serial1.flush();
     }
   }
 }
